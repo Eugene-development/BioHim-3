85b2525eeeb96804d8bcef027bd1cedf0eba9f32
indexpro24@gmail.com <indexpro24@gmail.com>
2022-11-03 21:37:42 +0300

    c

---
+++ app/src/lib/components/footer/index.svelte
@@ -0,0 +1,34 @@
+		<footer class="bg-gray-50">
+			<div class="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
+				<nav class="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
+					<div class="px-5 py-2">
+						<a href="#1" class="text-base text-gray-700"> Главная </a>
+					</div>
+					<div class="px-5 py-2">
+						<a href="#2" class="text-base text-gray-700"> Производство </a>
+					</div>
+					<div class="px-5 py-2">
+						<a href="#3" class="text-base text-gray-700"> Информация </a>
+					</div>
+					<div class="px-5 py-2">
+						<a href="#4" class="text-base text-gray-700"> Применение </a>
+					</div>
+					<div class="px-5 py-2">
+						<a href="#5" class="text-base text-gray-700"> О нас </a>
+					</div>
+				</nav>
+
+				<p class="mt-8 text-center text-base text-gray-400">
+					<span>
+						&copy; 2015-2022. Компания ООО ТК "ФОРВАРД ЛАЙН"
+
+						<a
+							class="rounded-md pl-1 text-xs font-medium text-gray-400 sm:text-base md:text-lg"
+							href="tel:+74950808101">+7 (495) 080-81-01</a
+						>
+					</span>
+
+					<br />Обслуживание сайта 8 (495) 147-55-50
+				</p>
+			</div>
+		</footer>
--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script>
     import '../app.css';
+    import Footer from "$lib/components/footer/index.svelte"
     /** @type {import('./$types').LayoutData} */
     // export let data;
 </script>
 
 <slot />
+<Footer />
