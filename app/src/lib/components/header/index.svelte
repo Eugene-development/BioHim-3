12c39ec10d9e313f3b1fbfffe9e719291792edaa
indexpro24@gmail.com <indexpro24@gmail.com>
2022-11-03 22:22:45 +0300

    c

---
+++ app/src/lib/components/header/index.svelte
@@ -0,0 +1,136 @@
+		<header class="relative">
+			<div class="bg-main py-2 border-b-2 border-cyan-100">
+				<nav
+					class="relative mx-auto flex max-w-full items-center justify-between px-4 py-2 sm:px-6 "
+					aria-label="Global"
+				>
+					<div class="flex flex-1 items-center">
+						<div class="flex w-full items-center justify-between lg:w-auto">
+							<a href="#1">
+								<span class="sr-only">Work</span>
+								<img
+									class="h-8 w-auto sm:h-10"
+									src="https://storage.yandexcloud.net/brand-logo/biohim/logo/logo.svg"
+									alt="logo"
+								/>
+							</a>
+							<div class="md:hidden -mr-2 flex items-center ">
+								<button
+									type="button"
+									class="focus-ring-inset inline-flex items-center justify-center rounded-md bg-gray-900 p-2 text-gray-400 hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-white"
+									aria-expanded="false"
+								>
+									<span class="sr-only">Open main menu</span>
+									<!-- Heroicon name: outline/bars-3 -->
+									<svg
+										class="h-6 w-6"
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										stroke-width="1.5"
+										stroke="currentColor"
+										aria-hidden="true"
+									>
+										<path
+											stroke-linecap="round"
+											stroke-linejoin="round"
+											d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+										/>
+									</svg>
+								</button>
+							</div>
+						</div>
+
+						<div class="hidden md:flex space-x-8 md:ml-8 ">
+							<a href="#1" class="text-base font-medium text-white hover:text-gray-300">Главная</a>
+
+							<a href="#2" class="text-base font-medium text-white hover:text-gray-300"
+								>Производство</a
+							>
+
+							<a href="#3" class="text-base font-medium text-white hover:text-gray-300"
+								>Информация</a
+							>
+
+							<a href="#4" class="text-base font-medium text-white hover:text-gray-300"
+								>Применение</a
+							>
+
+							<a href="#5" class="text-base font-medium text-white hover:text-gray-300"
+								>О&nbsp;нас
+							</a>
+						</div>
+					</div>
+					<div class="hidden xl:flex xl:items-center xl:space-x-6">
+						<div class="mr-2">
+							<div class=" flex items-center md:ml-2">
+								<div class="items-center">
+									<div class="flex items-center justify-between">
+										<div class="flex space-x-2 px-2 py-1 mx-1 rounded-md items-center antialiased">
+											<!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												class="h-6 w-6 text-cyan-400"
+												fill="none"
+												viewBox="0 0 24 24"
+												stroke="currentColor"
+												stroke-width="2"
+											>
+												<path
+													stroke-linecap="round"
+													stroke-linejoin="round"
+													d="M21 3l-6 6m0 0V4m0 5h5M5 3a2 2 0 00-2 2v1c0 8.284 6.716 15 15 15h1a2 2 0 002-2v-3.28a1 1 0 00-.684-.948l-4.493-1.498a1 1 0 00-1.21.502l-1.13 2.257a11.042 11.042 0 01-5.516-5.517l2.257-1.128a1 1 0 00.502-1.21L9.228 3.683A1 1 0 008.279 3H5z"
+												/>
+											</svg>
+											<span class="text-gray-50 text-xs sm:text-lg font-bold "
+												>+7 (495) 080-81-01</span
+											>
+											<!-- <a href="tel:+74950808101" class="text-gray-50 text-xs sm:text-lg font-bold "
+												>+7 (495) 080-81-01</a
+											> -->
+										</div>
+										<div class="flex space-x-2 px-2 py-1 mx-1 rounded-md items-center antialiased">
+											<!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												class="h-6 w-6 text-cyan-400"
+												fill="none"
+												viewBox="0 0 24 24"
+												stroke="currentColor"
+												stroke-width="2"
+											>
+												<path
+													stroke-linecap="round"
+													stroke-linejoin="round"
+													d="M3 19v-8.93a2 2 0 01.89-1.664l7-4.666a2 2 0 012.22 0l7 4.666A2 2 0 0121 10.07V19M3 19a2 2 0 002 2h14a2 2 0 002-2M3 19l6.75-4.5M21 19l-6.75-4.5M3 10l6.75 4.5M21 10l-6.75 4.5m0 0l-1.14.76a2 2 0 01-2.22 0l-1.14-.76"
+												/>
+											</svg>
+											<p class="text-gray-50 text-xs sm:text-xl font-bold">biohim.org@yandex.ru</p>
+										</div>
+									</div>
+								</div>
+
+								<!--												                        <button type="button" class="bg-gray-800 p-1 rounded-full text-cyan-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white animate-pulse">-->
+								<!--												                            <span class="sr-only">View notifications</span>-->
+								<!--												                            &lt;!&ndash; Heroicon name: outline/bell &ndash;&gt;-->
+								<!--												                            <svg class="h-6 w-6 " xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">-->
+								<!--												                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />-->
+								<!--												                            </svg>-->
+								<!--												                        </button>-->
+							</div>
+						</div>
+					</div>
+				</nav>
+			</div>
+
+			<!--
+              Mobile menu, show/hide based on menu open state.
+
+              Entering: "duration-150 ease-out"
+                From: "opacity-0 scale-95"
+                To: "opacity-100 scale-100"
+              Leaving: "duration-100 ease-in"
+                From: "opacity-100 scale-100"
+                To: "opacity-0 scale-95"
+            -->
+		</header>
--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script>
     import '../app.css';
     import Footer from "$lib/components/footer/index.svelte"
+    import Header from "$lib/components/header/index.svelte"
     /** @type {import('./$types').LayoutData} */
     // export let data;
 </script>
 
-<slot />
+<Header/>
+    <slot />
 <Footer />
