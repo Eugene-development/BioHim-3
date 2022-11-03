53e43bdcac96f1835f2bbf02cb75371f5062d079
indexpro24@gmail.com <indexpro24@gmail.com>
2022-11-03 22:39:06 +0300

    c

---
+++ app/src/lib/components/main/index.svelte
@@ -0,0 +1,217 @@
+			<div  class="bg-[url('https://storage.yandexcloud.net/brand-logo/biohim/foto/bg.png')] sm:pt-4 sm:pb-36 md:pb-24 lg:pb-0 lg:overflow-hidden ">
+				<div class="mx-auto max-w-7xl lg:px-8">
+					<div class="lg:grid lg:grid-cols-2 lg:gap-4">
+						<div
+							class="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 sm:text-center lg:flex lg:items-center lg:px-0 lg:text-left"
+						>
+							<div class=" lg:py-24 py-12 bg-main bg-opacity-60 px-2">
+								<span
+									class="inline-flex items-center rounded-full bg-black p-1.5 pr-2 text-white hover:text-gray-200 sm:text-base lg:text-sm xl:text-base"
+								>
+									<span
+										class="rounded-full bg-gradient-to-br from-blue-500 to-green-600 px-3 py-1 text-xs sm:text-sm font-semibold leading-5 text-white"
+										>Перекись водорода</span
+									>
+									<span class="ml-4 text-xs sm:text-sm">по оптовым ценам</span>
+									<!-- Heroicon name: mini/chevron-right -->
+									<svg
+										aria-hidden="true"
+										class="ml-2 h-5 w-5 text-gray-700"
+										fill="currentColor"
+										viewBox="0 0 20 20"
+										xmlns="http://www.w3.org/2000/svg"
+									>
+										<path
+											clip-rule="evenodd"
+											d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
+											fill-rule="evenodd"
+										/>
+									</svg>
+								</span>
+								<h1
+									class="mt-4 text-2xl font-bold tracking-tight text-white sm:mt-5 sm:text-4xl lg:mt-6"
+								>
+									<span class="block text-cyan-100">ООО ТК "ФОРВАРД ЛАЙН"</span>
+									<!--                                    <span class="block bg-gradient-to-r from-teal-200 to-cyan-400 bg-clip-text pb-3 text-transparent sm:pb-5">Н<span class="text-lg">2</span>O<span class="text-lg">2</span></span>-->
+								</h1>
+								<!-- TODO moderator -->
+								<p class="mt-8 text-base text-gray-300 sm:text-xl lg:text-lg xl:text-xl">
+								</p>
+
+								<div class=" py-12">
+									<form
+										class="sm:mx-auto sm:max-w-xl lg:mx-0"
+									>
+										<div class="my-3">
+											<div class="relative mt-1">
+												<button
+													required
+													type="button"
+													class="relative w-full cursor-default rounded-md border border-gray-300 bg-white px-4 py-3 text-left shadow-sm focus:border-cyan-500 focus:outline-none focus:ring-1 focus:ring-cyan-500 sm:text-base text-gray-800"
+													aria-haspopup="listbox"
+													aria-expanded="true"
+													aria-labelledby="listbox-label"
+												>
+													<span class="block truncate"></span>
+													<span
+														class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2"
+													>
+														<!-- Heroicon name: mini/chevron-up-down -->
+														<svg
+															class="h-5 w-5 text-gray-400"
+															xmlns="http://www.w3.org/2000/svg"
+															viewBox="0 0 20 20"
+															fill="currentColor"
+															aria-hidden="true"
+														>
+															<path
+																fill-rule="evenodd"
+																d="M10 3a.75.75 0 01.55.24l3.25 3.5a.75.75 0 11-1.1 1.02L10 4.852 7.3 7.76a.75.75 0 01-1.1-1.02l3.25-3.5A.75.75 0 0110 3zm-3.76 9.2a.75.75 0 011.06.04l2.7 2.908 2.7-2.908a.75.75 0 111.1 1.02l-3.25 3.5a.75.75 0 01-1.1 0l-3.25-3.5a.75.75 0 01.04-1.06z"
+																clip-rule="evenodd"
+															/>
+														</svg>
+													</span>
+												</button>
+
+												<!-- {#if $selectBrand}
+													<div on:outclick={closeVisibleSelected} use:clickOutside>
+														<ul
+															class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm border-2 border-cyan-500"
+															tabindex="-1"
+															role="listbox"
+															aria-labelledby="listbox-label"
+															aria-activedescendant="listbox-option-3"
+														>
+															{#each brands as { value }, i}
+																<li
+																	class="text-gray-900 hover:text-white hover:bg-cyan-600 relative cursor-default select-none py-2 pl-3 pr-9"
+																	id="listbox-option-0"
+																	role="option"
+																>
+																	<button
+																		class="w-full text-left"
+																		on:click={(e) => {
+																			brandSelected = e.target.innerText;
+																			changeVisibleSelectBrand();
+																		}}
+																	>
+																		<span class="font-normal block truncate">{value}</span>
+																		<span
+																			class="text-white absolute inset-y-0 right-0 flex items-center pr-4"
+																		>
+																			<svg
+																				class="h-5 w-5"
+																				xmlns="http://www.w3.org/2000/svg"
+																				viewBox="0 0 20 20"
+																				fill="currentColor"
+																				aria-hidden="true"
+																			>
+																				<path
+																					fill-rule="evenodd"
+																					d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
+																					clip-rule="evenodd"
+																				/>
+																			</svg>
+																		</span>
+																	</button>
+																</li>
+															{/each}
+														</ul>
+													</div>
+												{/if} -->
+											</div>
+										</div>
+										<div class="my-3">
+											<div class="relative mt-1">
+												<button
+													required
+													type="button"
+													class="relative w-full cursor-default rounded-md border border-gray-300 bg-white  px-4 py-3 text-left shadow-sm focus:border-cyan-500 focus:outline-none focus:ring-1 focus:ring-cyan-500 sm:text-base text-gray-800"
+													aria-haspopup="listbox"
+													aria-expanded="true"
+													aria-labelledby="listbox-label"
+												>
+													<!-- <span class="block truncate">{containerSelected}</span> -->
+													<span
+														class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2"
+													>
+														<!-- Heroicon name: mini/chevron-up-down -->
+														<svg
+															class="h-5 w-5 text-gray-400"
+															xmlns="http://www.w3.org/2000/svg"
+															viewBox="0 0 20 20"
+															fill="currentColor"
+															aria-hidden="true"
+														>
+															<path
+																fill-rule="evenodd"
+																d="M10 3a.75.75 0 01.55.24l3.25 3.5a.75.75 0 11-1.1 1.02L10 4.852 7.3 7.76a.75.75 0 01-1.1-1.02l3.25-3.5A.75.75 0 0110 3zm-3.76 9.2a.75.75 0 011.06.04l2.7 2.908 2.7-2.908a.75.75 0 111.1 1.02l-3.25 3.5a.75.75 0 01-1.1 0l-3.25-3.5a.75.75 0 01.04-1.06z"
+																clip-rule="evenodd"
+															/>
+														</svg>
+													</span>
+												</button>
+
+											</div>
+										</div>
+
+										<div class="my-3">
+											<label for="volume" class="sr-only">Желаемый объём</label>
+											<input
+												required
+												class="block w-full rounded-md border-0 px-4 py-3 text-base text-gray-900 placeholder-gray-800 focus:outline-none focus:ring-2 focus:ring-cyan-400 focus:ring-offset-2 focus:ring-offset-gray-900"
+												id="volume"
+												placeholder="Желаемый объём"
+												type="text"
+											/>
+										</div>
+
+										<div class="sm:flex">
+											{#if true}
+												<div class="min-w-0 flex-1">
+													<label for="email" class="sr-only">Ваша почта</label>
+													<input
+														required
+														class="block w-full rounded-md border-0 px-4 py-3 text-base text-gray-900 placeholder-gray-800 focus:outline-none focus:ring-2 focus:ring-cyan-400 focus:ring-offset-2 focus:ring-offset-gray-900"
+														id="email"
+														placeholder="Ваша почта"
+														type="email"
+													/>
+												</div>
+
+												<div class="mt-3 sm:mt-0 sm:ml-3">
+													<button
+														class="block w-full rounded-md bg-gradient-to-br from-blue-500 to-green-600 py-3 px-4 font-medium text-white shadow hover:from-teal-600 hover:to-cyan-700 focus:outline-none focus:ring-2 focus:ring-cyan-400 focus:ring-offset-2 focus:ring-offset-gray-900"
+														type="submit"
+													>
+														Отправить запрос на цену
+													</button>
+												</div>
+											{:else}
+												<span
+													class="block w-full rounded-md bg-gradient-to-br from-blue-500 to-green-600 py-3 px-4 font-medium text-white shadow hover:from-teal-600 hover:to-cyan-700 focus:outline-none focus:ring-2 focus:ring-cyan-400 focus:ring-offset-2 focus:ring-offset-gray-900"
+												>
+													Заявка отправлена. Ожидайте ответа на почту.
+												</span>
+											{/if}
+										</div>
+										<p class="mt-3 text-sm text-gray-300 sm:mt-4">
+											Отправьте ваш запрос и получите актуальную цену на перекись водорода.
+										</p>
+									</form>
+								</div>
+							</div>
+						</div>
+						<div class="mt-12 -mb-16 sm:-mb-48 lg:relative lg:m-0">
+							<div class="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:max-w-none lg:px-0">
+								<!-- Illustration taken from Lucid Illustrations: https://lucid.pixsellz.io/ -->
+								<img
+									class="w-full lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
+									src=""
+									alt=""
+								/>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
